4ce551d78a8d69ca442cfc6901212fd4ec16065a
MartimSiopa <shark09141@sharkcoders.pt>
2025-10-01 18:12:17 +0100

    Add files via upload

---
+++ medias.py
@@ -0,0 +1,8 @@
+#calcular as medias de 3 números introduzidos pelo utilizador
+
+a=int(input("diz me um número:"))
+b=int(input("diz outro número:"))
+c=int(input("diz me número final:"))
+
+media=(a+b+c)/3
+print(media)
