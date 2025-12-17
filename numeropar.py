234d3788451f211645543198753a3d3a9ab199bf
MartimSiopa <shark09141@sharkcoders.pt>
2025-12-17 17:35:58 +0000

    Add files via upload

--- numeropar.py
+++ numeropar.py
@@ -1,11 +1,21 @@
 
 num_inicial=int(input("diga o numero inicial"))
 num_final= int(input("Diga o numero final"))
-incremento=2
 
-if num_inicial%2==0:
-  for i in range(num_inicial,num_final +  1,incremento):
-    print(i)
-else:
-    for i in range(num_inicial+1, num_final + 1, incremento):
-        print(i)
+
+
+decisao= input("Par (P) Impar (I)")
+if decisao == "P":
+    if num_inicial%2==0:
+        for i in range(num_inicial,num_final + 1,2):
+            print(i)
+    else:
+        for i in range(num_inicial+1, num_final + 1,2):
+            print(i)
+elif decisao == "I":
+    if num_inicial%2 != 0:
+        for i in range(num_inicial + 1, num_final + 1,2):
+            print(i)
+    else:
+        for i in range(num_inicial+1, num_final + 1,2):
+            print(i)
