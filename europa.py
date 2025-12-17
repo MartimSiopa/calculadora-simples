3535fffd3efcbd9692b1a53ab11d94748956ca57
MartimSiopa <shark09141@sharkcoders.pt>
2025-12-17 17:53:08 +0000

    Add files via upload

---
+++ europa.py
@@ -0,0 +1,7 @@
+paises = ["Portugal","França","Alemanha","Itália","Ucrânia","Inglaterra",]
+tentativa = input("Diga um país da europa")
+
+for i in paises:
+    if i == tentativa:
+        print("ganhou")
+print("se nao ganhou ate agora perdeu")
